9469db340cc6f053416f447d601b543f12ce0522
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-05-01 21:51:07 +0100

    Library can count books by genre

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,4 +1,7 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 
 public class Library {
     private ArrayList<Book> bookCollection;
@@ -24,7 +27,33 @@ public class Library {
     }
 
     public Book lendBook(int index) {
-
         return this.bookCollection.remove(index);
     }
+
+    private HashMap booksByGenre() {
+        ArrayList<String> genres = new ArrayList<>();
+        HashMap<String, Integer> booksByGenre = new HashMap<>();
+
+        for (Book book : bookCollection) {
+            if (!genres.contains(book.getGenre())) {
+                genres.add(book.getGenre());
+            }
+        }
+
+        for (String genre : genres) {
+            int counter = 0;
+            for (Book book : bookCollection) {
+                if (book.getGenre().equals(genre)) {
+                    counter += 1;
+                }
+            }
+            booksByGenre.put(genre, counter);
+        }
+        return booksByGenre;
+    }
+
+    public int bookCountByGenre(String genre) {
+        return booksByGenre().get(genre).hashCode();
+    }
+
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -7,11 +7,15 @@ import static org.junit.Assert.assertEquals;
 public class LibraryTest {
     Library testLibrary;
     Book book;
+    Book book2;
+    Book book3;
 
     @Before
     public void before() {
         testLibrary = new Library();
-        book = new Book("A Book", "Paper");
+        book = new Book("A Book", "Thriller");
+        book2 = new Book("Another Book", "Crime");
+        book3 = new Book("One More Book", "Thriller");
     }
 
     @Test
@@ -39,6 +43,15 @@ public class LibraryTest {
     public void canLendBook() {
         testLibrary.addBook(book);
         assertEquals(book, testLibrary.lendBook(0));
+    }
+
+    @Test
+    public void countBooksByGenre() {
+        testLibrary.addBook(book);
+        testLibrary.addBook(book2);
+        testLibrary.addBook(book3);
+        assertEquals(2, testLibrary.bookCountByGenre("Thriller"));
+        assertEquals(1, testLibrary.bookCountByGenre("Crime"));
     }
 
 }
